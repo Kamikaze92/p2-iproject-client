e5b29e39120b732628fbae6c52b6bd4d08192ba5
Kamikaze92 <mrxatob@gmail.com>
2021-11-17 22:46:35 +0700

    fixing home and slicing card

--- study-hall/src/views/Forum.vue
+++ study-hall/src/views/Forum.vue
@@ -10,55 +10,7 @@
                 <p class="card-text">Fisika adalah hal yang menyenangkan?</p>
             </div>
           </div>
-          <div class="card-body">
-            <h5 class="card-title">Budi</h5>
-            <p class="card-text">
-              Tentu fisika adalah hal yang menyenangkan namun itu juga sulit
-            </p>
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Andhika</h5>
-            <p class="card-text">Aku heran ada yang suka fisika</p>
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Alex</h5>
-            <p class="card-text">Fisika sangat sulit</p>
-          </div>
-          <div>
-            <form>
-              <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label"
-                  >Answer This Question</label
-                >
-                <textarea
-                  class="form-control"
-                  id="exampleFormControlTextarea1"
-                  rows="3"
-                ></textarea>
-                <button class="btn btn-primary mt-2">Submit</button>
-              </div>
-            </form>
-          </div>
-        </div>
-        <div class="card px-3" style="width: 40rem">
-          <div class="card-body">
-            <h5 class="card-title">Asep</h5>
-            <p class="card-text">Fisika adalah hal yang menyenangkan?</p>
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Budi</h5>
-            <p class="card-text">
-              Tentu fisika adalah hal yang menyenangkan namun itu juga sulit
-            </p>
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Andhika</h5>
-            <p class="card-text">Aku heran ada yang suka fisika</p>
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Alex</h5>
-            <p class="card-text">Fisika sangat sulit</p>
-          </div>
+          <CommentCard />
           <div>
             <form>
               <div class="mb-3">
@@ -82,10 +34,12 @@
 
 <script>
 import Navbar from "../components/Navbar.vue";
+import CommentCard from "../components/CommentCard.vue";
 export default {
   name: "Forum",
   components: {
     Navbar,
+    CommentCard
   },
 };
 </script>
